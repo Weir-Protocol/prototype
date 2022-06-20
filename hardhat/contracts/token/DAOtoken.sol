6405518f92ef8c9ae1eef0490553e176bc6a6059
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2022-06-20 22:04:16 +0530

    Updated contracts

--- hardhat/contracts/token/DAOtoken.sol
+++ hardhat/contracts/token/DAOtoken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DAOtoken is ERC20 {
-    constructor() ERC20("DAOtoken", "SDT") {
+    constructor() ERC20("Sample DAOtoken", "SDT") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
--- hardhat/contracts/token/Stablecoin.sol
+++ hardhat/contracts/token/Stablecoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Stablecoin is ERC20 {
-    constructor() ERC20("Stablecoin", "tUSD") {
+    constructor() ERC20("Sample Stablecoin", "tUSD") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
